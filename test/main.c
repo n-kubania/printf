d3ba050a79666fcff0937100cbc24e6e89462d22
James-chege <chegejames11.cj@gmail.com>
2023-08-22 19:01:29 +0300

    Compilation files

--- test/main.c
+++ test/main.c
@@ -3,42 +3,46 @@
 #include "../printf.h"
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	    int len;
-	        int len2;
-		    unsigned int ui;
-		        void *addr;
-
-			    len = _printf("Let's try to printf a simple sentence.\n");
-			        len2 = printf("Let's try to printf a simple sentence.\n");
-				    ui = (unsigned int)INT_MAX + 1024;
-				        addr = (void *)0x7ffe637541f0;
-					    _printf("Length:[%d, %i]\n", len, len);
-					        printf("Length:[%d, %i]\n", len2, len2);
-						    _printf("Negative:[%d]\n", -762534);
-						        printf("Negative:[%d]\n", -762534);
-							    _printf("Unsigned:[%u]\n", ui);
-							        printf("Unsigned:[%u]\n", ui);
-								    _printf("Unsigned octal:[%o]\n", ui);
-								        printf("Unsigned octal:[%o]\n", ui);
-									    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-									        printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-										    _printf("Character:[%c]\n", 'H');
-										        printf("Character:[%c]\n", 'H');
-											    _printf("String:[%s]\n", "I am a string !");
-											        printf("String:[%s]\n", "I am a string !");
-												    _printf("Address:[%p]\n", addr);
-												        printf("Address:[%p]\n", addr);
-													    len = _printf("Percent:[%%]\n");
-													        len2 = printf("Percent:[%%]\n");
-														    _printf("Len:[%d]\n", len);
-														        printf("Len:[%d]\n", len2);
-															    _printf("Unknown:[%r]\n");
-															        printf("Unknown:[%r]\n");
-																    return (0);
+	char gender = 'M';
+	int age = 10;
+	char name[] = "Kamanja";
+	int n;
+		
+	_printf("The boy by the name %s is here\n", name);
+	printf("The boy by the name %s is here\n", name);
+	_printf("The gender filled in his form is %c\n", gender);
+	printf("The gender filled in his form is %c\n", gender);
+	_printf("The young man is %d years old\n", age);
+	 printf("The young man is %d years old\n", age);
+	
+	for (n = 0; n <= 20; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			_printf("Nkatha&Chege");
+			_printf(" ");
+		}
+		else if (n % 3 == 0)
+		{
+			_printf("Nkatha");
+			_printf(" ");
+		}
+		else if (n % 5 == 0)
+		{
+			_printf("Chege");
+			_printf(" ");
+		}
+		else
+		{
+			_printf("%d ", n);
+		}
+	}
+	_printf("\n");	
+	return (0);
 }
